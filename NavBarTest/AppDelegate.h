21ad620963c66ad9c027540d37dc233016590fc0
Pascal Drouilly <bymyapp@drouilly.net>
2012-04-24 18:38:06 +0200

    Initial Commit

---
+++ NavBarTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NavBarTest
+//
+//  Created by Pascal Drouilly on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NavBarTest/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  NavBarTest
+//
+//  Created by Pascal Drouilly on 4/24/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
